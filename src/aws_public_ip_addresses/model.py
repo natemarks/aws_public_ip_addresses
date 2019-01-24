b941cece8fb43affc372752c121a8261cee73867
Nate Marks <nmarks@nwnit.com>
2019-01-24 08:09:33 -0500

    passing test_get_regions

--- src/aws_public_ip_addresses/model.py
+++ src/aws_public_ip_addresses/model.py
@@ -1,5 +1,8 @@
+from typing import List, Dict, Any
+
+
 class AWSAddresses(object):
-    """The summary line for a class docstring should fit on one line.
+    """AWS Public IP Addresses
 
     If the class has public attributes, they may be documented here
     in an ``Attributes`` section and follow the same formatting as a
@@ -8,17 +11,77 @@ class AWSAddresses(object):
 
     """
     json_download_url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'  # type: str
+    ipv4_prefix_key = 'prefixes'  # type: str
+    ipv6_prefix_key = 'ipv6_prefixes'  # type: str
 
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = {}
         self.kwargs.update(kwargs)
+        self._regions = None  # type: List[str]
+        self._download_data = None
+        self._ipv4_prefixes = None  # type: List[Dict[str, str]]
+        self._ipv6_prefixes = None  # type: List[Dict[str, str]]
 
     @classmethod
-    def get_address_data(cls):
-        import urllib2
+    def download_and_get_data(cls) -> Dict[str, Any]:
+        """Force download and return the json as nested dict
+
+        """
+        from urllib.request import urlopen
         import json
 
-        json_data = urllib2.urlopen(AWSAddresses.json_download_url).read()
+        json_data = urlopen(AWSAddresses.json_download_url).read()
 
         return json.loads(json_data)
+
+    def get_data(self):
+        if not self._download_data:
+            self._download_data = AWSAddresses.download_and_get_data()
+        return self._download_data
+
+    def get_ipv4_prefixes(self) -> List[Dict[str, str]]:
+        """The summary line for a method docstring should fit on one line.
+
+        returns the ipv4 prefixes dict
+        """
+        if not self._ipv4_prefixes:
+            self._ipv4_prefixes = self.get_data()[AWSAddresses.ipv4_prefix_key]
+        return self._ipv4_prefixes
+
+    def get_ipv6_prefixes(self) -> List[Dict[str, str]]:
+        """The summary line for a method docstring should fit on one line.
+
+        returns the ipv6 prefixes dict
+        """
+        if not self._ipv6_prefixes:
+            self._ipv6_prefixes = self.get_data()[AWSAddresses.ipv6_prefix_key]
+        return self._ipv6_prefixes
+
+    def get_regions(self, ip_version: int = None) -> List[str]:
+        """Return a list of the regions
+
+        if ip_version is left default (None), the method will return a list of all unique reqions from both the ipv4
+        and ipv6 prefix dicts,  If ipversion=4 is specified, it will just use the ipv4 prefix dict and if ipversion=6
+        is specified it'll just use the ipv6 dict
+
+        """
+        if not self._regions:
+            region_list = []
+            if ip_version in [4, None]:
+                for prefix in self.get_ipv4_prefixes():
+                    if prefix['region'] not in region_list:
+                        region_list.append(prefix['region'])
+
+            if ip_version in [6, None]:
+                for prefix in self.get_ipv6_prefixes():
+                    if prefix['region'] not in region_list:
+                        region_list.append(prefix['region'])
+
+            region_list.sort()
+            self._regions = region_list
+
+        return self._regions
+
+
+
---
+++ tests/test_model.py
@@ -0,0 +1,56 @@
+from aws_public_ip_addresses.model import AWSAddresses
+
+
+def test_check_download():
+    """
+    We expect the 'prefixes' key to exist at the top of the json download object
+    """
+    assert 'prefixes' in AWSAddresses.download_and_get_data()
+
+
+def test_get_data():
+    addresses = AWSAddresses()
+    output = addresses.get_data()
+    assert 'prefixes' in output
+
+
+def test_get_ipv4_prefixes():
+    addresses = AWSAddresses()
+    assert len(addresses.get_ipv4_prefixes()) > 0
+
+
+def test_get_ipv6_prefixes():
+    addresses = AWSAddresses()
+    assert len(addresses.get_ipv6_prefixes()) > 0
+
+
+def test_get_regions():
+    """
+    :return:
+    """
+    addresses = AWSAddresses()
+
+    all_regions = ['GLOBAL', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-southeast-1',
+                   'ap-southeast-2', 'ca-central-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-north-1',
+                   'eu-west-1', 'eu-west-2', 'eu-west-3', 'me-south-1', 'sa-east-1', 'us-east-1', 'us-east-2',
+                   'us-gov-east-1', 'us-gov-west-1', 'us-west-1', 'us-west-2']
+
+    all_regions.sort()
+
+    ipv4_regions = ['GLOBAL', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-southeast-1',
+                    'ap-southeast-2', 'ca-central-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-north-1',
+                    'eu-west-1', 'eu-west-2', 'eu-west-3', 'me-south-1', 'sa-east-1', 'us-east-1', 'us-east-2',
+                    'us-gov-east-1', 'us-gov-west-1', 'us-west-1', 'us-west-2']
+
+    ipv4_regions.sort()
+
+    ipv6_regions = ['GLOBAL', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-southeast-1',
+                    'ap-southeast-2', 'ca-central-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-north-1',
+                    'eu-west-1', 'eu-west-2', 'eu-west-3', 'me-south-1', 'sa-east-1', 'us-east-1', 'us-east-2',
+                    'us-gov-east-1', 'us-gov-west-1', 'us-west-1', 'us-west-2']
+
+    ipv6_regions.sort()
+
+    assert all_regions == addresses.get_regions()
+    assert ipv4_regions == addresses.get_regions(ip_version=4)
+    assert ipv6_regions == addresses.get_regions(ip_version=6)
